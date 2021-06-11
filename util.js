06b472278c9f93a5ee13c4718f91fe89ed0c2801
Lane Harris <lrharris215@gmail.com>
2021-06-11 14:50:21 -0400

    added jsonReader and jsonWriter functions to util

--- util.js
+++ util.js
@@ -1,5 +1,12 @@
 import * as readline from 'readline';
+import * as fs from 'fs';
+//import chalk from 'chalk';
 
+export const errorLog = (error) => {
+    //chalk this later!!!
+    const eLog = `Error: ${error}`;
+    console.log(eLog);
+};
 export const prompt = (question) => {
     const r = readline.createInterface({
         input: process.stdin,
@@ -11,5 +18,29 @@ export const prompt = (question) => {
             r.close();
             resolve(answer);
         });
+    });
+};
+
+export const jsonReader = (filePath, callback) => {
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            return callback && callback(err);
+        }
+        try {
+            const object = JSON.parse(data);
+            return callback && callback(null, object);
+        } catch (err) {
+            return callback && callback(err);
+        }
+    });
+};
+
+export const jsonWriter = (filePath, data) => {
+    fs.writeFile(filePath, JSON.stringify(data), (err) => {
+        if (err) {
+            errorLog(err);
+        } else {
+            console.log('Writing to JSON succeeded!');
+        }
     });
 };
