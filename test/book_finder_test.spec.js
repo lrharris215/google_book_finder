88fd2debcd324a555485361c48304eee7d094a8f
Lane Harris <lrharris215@gmail.com>
2021-06-24 15:21:03 -0400

    Wrote out list of tests I will need for book_finder.js

---
+++ test/book_finder_test.spec.js
@@ -0,0 +1,19 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+
+describe('getUserInput()', () => {
+    it('gets the users input', () => {});
+});
+
+describe('showHelp()', () => {
+    it('prints help to the console');
+});
+
+describe('fetchBooks(searchTerm)', () => {});
+
+describe('formatBooks()');
+describe('printBooks');
+
+describe('saveBook');
+describe('fetchReadingList');
+describe('viewReadingList');
